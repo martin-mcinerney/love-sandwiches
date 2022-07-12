6b200112a1564b046663d1f52b1e9add1df9d512
martin-mcinerney <martin_mcinerney@live.ie>
2022-07-12 14:04:16 +0100

    Adds functions that validate user input

--- run.py
+++ run.py
@@ -16,8 +16,28 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Love Sandwiches')
 
-sales = SHEET.worksheet('sales')
+def get_sales_data():
+    """
+    Get sales input from user
+    """
+    print("Please enter sales data from last market.")
+    print("Data should be 6 numbers seperated by commas.")
+    print("Example: 10,23,45,67,43,23\n")
+    data_str = input('Enter your data here: ')
+    sales_data= data_str.split(',')
+    print(sales_data)
+    validate_data(sales_data)
 
-data = sales.get_all_values()
+def validate_data(values):
+    """
+    Inside the try, turns all strings into integers and raises an error if this cannot be completed or there aren't exactly 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required. You provided {len(values)}"
+            )
+    except ValueError as e: 
+        print(f'Invalid data: {e}, please try again.\n')
 
-print(data)
+get_sales_data()
